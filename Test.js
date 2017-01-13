b7d795b31ba2f90978fc6e258a435c360176c758
CaramelYo <alltime326@gmail.com>
2017-01-13 21:51:35 +0800

    Delete redundant data

--- Test.js
+++
@@ -1,9 +0,0 @@
-
-console.log("YO");
-audioFile = "chimes.wav";
-shell = new ActiveXObject("Wscript.Shell");
-//command = "sndrec32 /play /close \"" + audioFile + "\"";
-command = "soundRecorder /play /close \"" + audioFile + "\"";
-command = "start " + audioFile;
-WScript.echo(command);
-shell.Run(command, 0);
--- Test.py
+++
@@ -1,2 +0,0 @@
-print("YO")
-print("YOOO")
