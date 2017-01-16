04f94e41bc8dc458578d9937fe522d9bcad2fb5c
CaramelYo <alltime326@gmail.com>
2017-01-16 14:52:06 +0800

    remove redundant code and file

--- my_site/Demo/test1.py
+++ my_site/Demo/test1.py
@@ -1,17 +1,8 @@
-def sen():
-    import numpy as np
-    from bokeh.io import vform
-    from bokeh.models import CustomJS, ColumnDataSource, Slider
-    from bokeh.models.widgets import TextInput
-    from bokeh.layouts import gridplot
-    from bokeh.plotting import figure, show, output_file
-    from bokeh.embed import components
-    from bokeh.resources import CDN
+def func():
+    return 'YO', 'YOO', 'YOOO'
 
-    import twittertrend
+t1, t2, t3 = func()
 
-    result, hashtable = twittertrend.trend(number = 10, lag = 0)
-    
-    print(result)
+print(t1)
+print(t2)
 
-sen()
